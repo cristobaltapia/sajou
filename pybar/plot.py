62461f76e7f13823f46c9efc0021a235b0b03e0d
Cristobal Tapia Camu <crtapia@gmail.com>
2016-09-06 11:01:20 +0200

    Improved plotting of member forces

--- pybar/plot.py
+++ pybar/plot.py
@@ -4,8 +4,9 @@
 # This fil defines all the functions needed to plot the created structures and the results
 # obtained with the program.
 import numpy as np
-import matplotlib as pyplot
+import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
+from matplotlib.axes import Axes
 
 def plot_geometry(model, ax):
     """Plots the geometry of the model passed
@@ -41,20 +42,43 @@ def plot_internal_forces(model, ax, result, component, scale):
     """
     # First plot the geometry
     ax = plot_geometry(model, ax)
+    # auxiliary axes
+    fig_aux = plt.figure()
+    ax_aux = fig_aux.add_subplot(111)
     # Plot the specified diagram
     # Transform the results from the local coordinates to global
     # coordinates
     for num, elem in model.beams.items():
+        # Get the transformation matrix
         T = elem.transformation_matrix[0:2,0:2]
+        # Get the specified member forces
         x_axis = result.internal_forces[num]['x']
-        axial = result.internal_forces[num][component]*scale
-        diag = np.vstack([x_axis, axial])
-        diag = np.dot(diag.T, T)
+        d = result.internal_forces[num][component]*scale
 
-        x = np.hstack([elem._node1.x, diag.T[0] + elem._node1.x, elem._node2.x])
-        d = np.hstack([elem._node1.y, diag.T[1] + elem._node1.y, elem._node2.y])
-        polygon = Polygon(np.vstack([x,d]).T, True, facecolor='red', alpha=0.5)
-        #ax.plot(x, d)
-        ax.add_patch(polygon)
+        # Positive values
+        d_pos = ax_aux.fill_between(x_axis, d, 0, where=d>0, interpolate=True)
+        d_pos = d_pos.get_paths()
+        # Negative values
+        d_neg = ax_aux.fill_between(x_axis, d, 0, where=d<0, interpolate=True)
+        d_neg = d_neg.get_paths()
+
+        # Plot the patches
+        # positive part
+        for curr_pol in d_pos:
+            rot_pos = np.dot(curr_pol.vertices, T)
+            rot_pos[:,0] += elem._node1.x
+            rot_pos[:,1] += elem._node1.y
+            poly_pos = Polygon(rot_pos, True, facecolor='blue', alpha=0.5)
+            ax.add_patch(poly_pos)
+
+        # negative part
+        for curr_pol in d_neg:
+            rot_neg = np.dot(curr_pol.vertices, T)
+            rot_neg[:,0] += elem._node1.x
+            rot_neg[:,1] += elem._node1.y
+            poly_neg = Polygon(rot_neg, True, facecolor='red', alpha=0.5)
+            ax.add_patch(poly_neg)
+
+    plt.close(fig_aux)
 
     return ax

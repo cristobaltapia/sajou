9be746c66e2cde49940e815c96ba074dd6f4fd6c
Cristóbal Tapia Camú <crtapia@gmail.com>
2016-10-29 18:22:30 +0200

    Move Material class to its own module

---
+++ pybar/materials.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" This module contains the classes and methods used for the creation of material
+properties.
+"""
+
+class Material(object):
+    """Material properties"""
+
+    # TODO: create function to print information of the material
+    def __init__(self, name, data, type='isotropic'):
+        """TODO: to be defined1.
+
+        :name: name of the material
+        :table: properties of the material
+        :type: type of the material:
+            - 'isotropic': data = (E, )
+            - 'orthotropic': data = (E_1, E_2, E_3)
+
+        """
+        self._name = name
+        self._data = data
+        self._type = type
+
+    def __str__(self):
+        """
+        Returns the printable string for this object
+        """
+        return 'Material: {name}'.format(name=self._name)
+
+    def __repr__(self):
+        """
+        Returns the printable string for this object
+        """
+        return 'Material: {name}'.format(name=self._name)
+
--- pybar/model.py
+++ pybar/model.py
@@ -619,36 +619,6 @@ class SymbolicModel2D(Model):
 
         return line
 
-class Material(object):
-    """Material properties"""
-
-    # TODO: create function to print information of the material
-    def __init__(self, name, data, type='isotropic'):
-        """TODO: to be defined1.
-
-        :name: name of the material
-        :table: properties of the material
-        :type: type of the material:
-            - 'isotropic': data = (E, )
-            - 'orthotropic': data = (E_1, E_2, E_3)
-
-        """
-        self._name = name
-        self._data = data
-        self._type = type
-
-    def __str__(self):
-        """
-        Returns the printable string for this object
-        """
-        return 'Material: {name}'.format(name=self._name)
-
-    def __repr__(self):
-        """
-        Returns the printable string for this object
-        """
-        return 'Material: {name}'.format(name=self._name)
-
 class BeamSection(object):
     """Defines a beam section"""
 

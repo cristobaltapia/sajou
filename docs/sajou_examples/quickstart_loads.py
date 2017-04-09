c5ec7665f9b3e7890532a4c39c36d9b9caa16cee
Cristóbal Tapia Camú <crtapia@gmail.com>
2017-04-09 22:02:23 +0200

    Change in the model used in the quickstart

--- docs/sajou_examples/quickstart_loads.py
+++ docs/sajou_examples/quickstart_loads.py
@@ -15,38 +15,42 @@ m = sj.Model(name='test model', dimensionality='2D')
 # add nodes
 n1 = m.node(0., 0.)
 n2 = m.node(0., 2000.)
-n3 = m.node(2000., 2000.)
-n4 = m.node(2000., 0.)
+n3 = m.node(1500., 2500.)
+n4 = m.node(3000., 2000.)
+n5 = m.node(3000., 0.)
 
 # add segment
 b1 = m.beam(node1=n1, node2=n2)
 b2 = m.beam(node1=n2, node2=n3)
 b3 = m.beam(node1=n3, node2=n4)
+b4 = m.beam(node1=n4, node2=n5)
 
 # create material
-mat = m.material(name='Wood', data=(200e3, ), type='isotropic')
+mat = m.material(name='Wood', data=(12e3, ), type='isotropic')
 
 # create beam section
 section1 = m.beam_section(name='rectangular 1', material=mat, data=(
-    32e2, 2356e4), type='general')
-section2 = m.beam_section(name='rectangular 2', material=mat, data=(
-    66e2, 5245e4), type='general')
+    300, 100), type='rectangular')
 
 # add beam section to the beams
 b1.assign_section(section1)
 b2.assign_section(section1)
-b3.assign_section(section2)
+b3.assign_section(section1)
 
 # Add border conditions
-m.bc(node=n1, v1=0., v2=0., r3=0.)
-m.bc(node=n4, v1=0., v2=0.)
+m.bc(node=n1, v1=0., v2=0.)
+m.bc(node=n5, v1=0., v2=0.)
 
 # Add load
-m.load(node=n3, f2=-5e3)
-m.load(node=n2, f2=-10e3)
+m.load(node=n4, f2=-10e3)
 
 # Distributed load
 b1.distributed_load(p1=-1, p2=-2, direction='y', coord_system='local')
+b2.distributed_load(p1=-1, direction='y', coord_system='global')
+b3.distributed_load(p1=-1, direction='y', coord_system='global')
+
+# release end
+b2.release_end(which=2)
 
 fig = plt.figure(figsize=(6., 5.5))
 ax = fig.add_subplot(111)
@@ -60,7 +64,7 @@ ax.spines['bottom'].set_smart_bounds(True)
 ax.spines['left'].set_smart_bounds(True)
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
-ax.set_xlim(xmin=-800, xmax=2500)
+ax.set_xlim(xmin=-800, xmax=3500)
 
 
 plt.tight_layout()
--- docs/sajou_examples/quickstart_post.py
+++ docs/sajou_examples/quickstart_post.py
@@ -16,38 +16,43 @@ m = sj.Model(name='test model', dimensionality='2D')
 # add nodes
 n1 = m.node(0., 0.)
 n2 = m.node(0., 2000.)
-n3 = m.node(2000., 2000.)
-n4 = m.node(2000., 0.)
+n3 = m.node(1500., 2500.)
+n4 = m.node(3000., 2000.)
+n5 = m.node(3000., 0.)
 
 # add segment
 b1 = m.beam(node1=n1, node2=n2)
 b2 = m.beam(node1=n2, node2=n3)
 b3 = m.beam(node1=n3, node2=n4)
+b4 = m.beam(node1=n4, node2=n5)
 
 # create material
-mat = m.material(name='Wood', data=(200e3, ), type='isotropic')
+mat = m.material(name='Wood', data=(12e3, ), type='isotropic')
 
 # create beam section
-section1 = m.beam_section(name='rectangular 1', material=mat,
-                          data=(32e2, 2356e4), type='general')
-section2 = m.beam_section(name='rectangular 2', material=mat,
-                          data=(66e2, 5245e4), type='general')
+section1 = m.beam_section(name='rectangular 1', material=mat, data=(
+    300, 100), type='rectangular')
 
 # add beam section to the beams
 b1.assign_section(section1)
 b2.assign_section(section1)
-b3.assign_section(section2)
+b3.assign_section(section1)
+b4.assign_section(section1)
 
 # Add border conditions
-m.bc(node=n1, v1=0., v2=0., r3=0.)
-m.bc(node=n4, v1=0., v2=0.)
+m.bc(node=n1, v1=0., v2=0.)
+m.bc(node=n5, v1=0., v2=0.)
 
 # Add load
-m.load(node=n3, f2=-5e3)
-m.load(node=n2, f2=-10e3)
+m.load(node=n4, f2=-10e3)
 
 # Distributed load
 b1.distributed_load(p1=-1, p2=-2, direction='y', coord_system='local')
+b2.distributed_load(p1=-1, direction='y', coord_system='global')
+b3.distributed_load(p1=-1, direction='y', coord_system='global')
+
+# release end
+b2.release_end(which=2)
 
 # Define output variables
 output = ['nodal displacements', 'internal forces', 'end forces']
@@ -72,7 +77,7 @@ ax1 = disp.plot_internal_forces(ax=ax1, result=res, component='moment')
 ax2 = disp.plot_internal_forces(ax=ax2, result=res, component='shear')
 ax3 = disp.plot_internal_forces(ax=ax3, result=res, component='axial')
 ax4 = disp.plot_deformed_geometry(ax=ax4, result=res, show_undeformed=True,
-                                  scale=500)
+                                  scale=10)
 
 for ax in [ax1, ax2, ax3, ax4]:
     ax.spines['right'].set_color('none')
@@ -81,8 +86,8 @@ for ax in [ax1, ax2, ax3, ax4]:
     ax.spines['left'].set_smart_bounds(True)
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
-    ax.set_xlim(xmin=-800, xmax=3000)
     ax.set_ylim(ymin=-500, ymax=2800)
+    ax.set_xlim(xmin=-800, xmax=4000)
 
 ax1.set_title('Moment diagram')
 ax2.set_title('Shear force diagram')
